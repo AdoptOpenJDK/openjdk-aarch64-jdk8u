e47a7bacdc71da488d66710e5b80ab9a8e17bb14
shade <unknown>
2017-11-20 21:39:35 +0100

    [backport] Enable biased locking for Shenandoah by default

--- src/share/vm/runtime/arguments.cpp
+++ src/share/vm/runtime/arguments.cpp
@@ -1785,14 +1785,6 @@ void Arguments::set_shenandoah_gc_flags() {
     FLAG_SET_DEFAULT(AlwaysPreTouch, false);
     FLAG_SET_DEFAULT(ShenandoahAlwaysPreTouch, true);
   }
-
-  // Current Hotspot machinery for biased locking may introduce lots of latency hiccups
-  // that negate the benefits of low-latency GC. The throughput improvements granted by
-  // biased locking on modern hardware are not covering the latency problems induced by
-  // it. Therefore, unless user really wants it, disable biased locking.
-  if (FLAG_IS_DEFAULT(UseBiasedLocking)) {
-    FLAG_SET_DEFAULT(UseBiasedLocking, false);
-  }
 }
 
 #if !INCLUDE_ALL_GCS
