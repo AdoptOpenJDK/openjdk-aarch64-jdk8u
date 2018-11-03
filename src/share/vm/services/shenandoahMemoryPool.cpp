feeba46b7d28ca9bcda3030160b43bb27f1e66fb
shade <unknown>
2018-11-03 23:59:21 +0100

    [backport] Initial committed regions count can be more than max

--- src/share/vm/services/shenandoahMemoryPool.cpp
+++ src/share/vm/services/shenandoahMemoryPool.cpp
@@ -33,9 +33,15 @@ ShenandoahMemoryPool::ShenandoahMemoryPool(ShenandoahHeap* heap) :
                             _heap(heap) {}
 
 MemoryUsage ShenandoahMemoryPool::get_memory_usage() {
-  size_t maxSize   = max_size();
+  size_t initial   = initial_size();
+  size_t max       = max_size();
   size_t used      = used_in_bytes();
   size_t committed = _heap->committed();
 
-  return MemoryUsage(initial_size(), used, committed, maxSize);
+  assert(initial <= max,    err_msg("initial: "   SIZE_FORMAT ", max: "       SIZE_FORMAT, initial,   max));
+  assert(used <= max,       err_msg("used: "      SIZE_FORMAT ", max: "       SIZE_FORMAT, used,      max));
+  assert(committed <= max,  err_msg("committed: " SIZE_FORMAT ", max: "       SIZE_FORMAT, committed, max));
+  assert(used <= committed, err_msg("used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed));
+
+  return MemoryUsage(initial, used, committed, max);
 }
