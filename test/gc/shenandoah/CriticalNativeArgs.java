4fa6aa4e136866a37ae2692ac6de9c5995311f82
rkennke <unknown>
2018-11-28 18:40:28 +0100

    [backport] Fix indentation of CriticalNative* tests

--- test/gc/shenandoah/CriticalNativeArgs.java
+++ test/gc/shenandoah/CriticalNativeArgs.java
@@ -22,21 +22,21 @@
  */
 
 public class CriticalNativeArgs {
-  static {
-    System.loadLibrary("CriticalNative");
-  }
-
-  static native boolean isNull(int[] a);
-
-  public static void main(String[] args) {
-    int[] arr = new int[2];
-
-    if (isNull(arr)) {
-      throw new RuntimeException("Should not be null");
+    static {
+        System.loadLibrary("CriticalNative");
     }
 
-    if (!isNull(null)) {
-      throw new RuntimeException("Should be null");
+    static native boolean isNull(int[] a);
+
+    public static void main(String[] args) {
+        int[] arr = new int[2];
+
+        if (isNull(arr)) {
+            throw new RuntimeException("Should not be null");
+        }
+
+        if (!isNull(null)) {
+            throw new RuntimeException("Should be null");
+        }
     }
-  }
 }
