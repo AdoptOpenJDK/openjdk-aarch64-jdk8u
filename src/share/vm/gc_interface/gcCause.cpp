f821929b5cd725d33e154753b23838067f4153d3
rkennke <unknown>
2018-12-04 12:13:19 +0100

    [backport] Re-label Shenandoah GC causes (SA&GC)

--- src/share/vm/gc_interface/gcCause.cpp
+++ src/share/vm/gc_interface/gcCause.cpp
@@ -101,16 +101,16 @@ const char* GCCause::to_string(GCCause::Cause cause) {
       return "G1 Humongous Allocation";
 
     case _shenandoah_allocation_failure_evac:
-      return "Allocation Failure During Evac";
+      return "Allocation Failure During Evacuation";
 
     case _shenandoah_stop_vm:
       return "Stopping VM";
 
     case _shenandoah_concurrent_gc:
-      return "Shenandoah Concurrent GC";
+      return "Concurrent GC";
 
     case _shenandoah_upgrade_to_full_gc:
-      return "Shenandoah Upgrade To Full GC";
+      return "Upgrade To Full GC";
 
     case _last_ditch_collection:
       return "Last ditch collection";
