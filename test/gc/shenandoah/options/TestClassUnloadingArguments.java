17ef4c3fa8926c261fa4f3c84261b453c2e5f586
shade <unknown>
2020-06-17 17:21:47 +0200

    [backport] 8247751: Shenandoah: options tests should run with smaller heaps Reviewed-by: zgu, rkennke

--- test/gc/shenandoah/options/TestClassUnloadingArguments.java
+++ test/gc/shenandoah/options/TestClassUnloadingArguments.java
@@ -37,9 +37,10 @@ import com.oracle.java.testlibrary.*;
 public class TestClassUnloadingArguments {
 
     public static void testWith(String msg, boolean cu, boolean cuConc, String... args) throws Exception {
-        String[] cmds = Arrays.copyOf(args, args.length + 2);
-        cmds[args.length] = "-XX:+PrintFlagsFinal";
-        cmds[args.length + 1] = "-version";
+        String[] cmds = Arrays.copyOf(args, args.length + 3);
+        cmds[args.length] = "-Xmx128m";
+        cmds[args.length + 1] = "-XX:+PrintFlagsFinal";
+        cmds[args.length + 2] = "-version";
         ProcessBuilder pb = ProcessTools.createJavaProcessBuilder(cmds);
         OutputAnalyzer output = new OutputAnalyzer(pb.start());
         output.shouldHaveExitValue(0);
