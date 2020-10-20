21e46b1a62a070215a688a9f6e4392c6469ddabc
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-21 05:34:52 +0700

    C++

--- jdk/src/share/native/common/bionic_mbstate.h
+++ jdk/src/share/native/common/bionic_mbstate.h
@@ -34,8 +34,8 @@ __BEGIN_DECLS
  * These return values are specified by POSIX for multibyte conversion
  * functions.
  */
-#define __MB_ERR_ILLEGAL_SEQUENCE (size_t)(-1)
-#define __MB_ERR_INCOMPLETE_SEQUENCE (size_t)(-2)
+#define __MB_ERR_ILLEGAL_SEQUENCE static_cast<size_t>(-1)
+#define __MB_ERR_INCOMPLETE_SEQUENCE static_cast<size_t>(-2)
 #define __MB_IS_ERR(rv) (rv == __MB_ERR_ILLEGAL_SEQUENCE || \
                          rv == __MB_ERR_INCOMPLETE_SEQUENCE)
 static inline __wur size_t mbstate_bytes_so_far(const mbstate_t* ps) {
@@ -45,18 +45,18 @@ static inline __wur size_t mbstate_bytes_so_far(const mbstate_t* ps) {
       (ps->__seq[0] != 0) ? 1 : 0;
 }
 static inline void mbstate_set_byte(mbstate_t* ps, int i, char byte) {
-  ps->__seq[i] = (uint8_t)(byte);
+  ps->__seq[i] = static_cast<uint8_t>(byte);
 }
 static inline __wur uint8_t mbstate_get_byte(const mbstate_t* ps, int n) {
   return ps->__seq[n];
 }
 static inline __wur size_t mbstate_reset_and_return_illegal(int _errno, mbstate_t* ps) {
   errno = _errno;
-  (uint32_t)(ps->__seq) = 0;
+  *(reinterpret_cast<uint32_t*>(ps->__seq)) = 0;
   return __MB_ERR_ILLEGAL_SEQUENCE;
 }
 static inline __wur size_t mbstate_reset_and_return(int _return, mbstate_t* ps) {
-  (uint32_t*)(ps->__seq) = 0;
+  *(reinterpret_cast<uint32_t*>(ps->__seq)) = 0;
   return _return;
 }
 __END_DECLS
