2c9d45f6a1cfb3e22398b11c2a9a57a96e973cb0
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-26 12:22:42 +0700

    Update os_linux.cpp

--- hotspot/src/os/linux/vm/os_linux.cpp
+++ hotspot/src/os/linux/vm/os_linux.cpp
@@ -114,10 +114,6 @@
 PRAGMA_FORMAT_MUTE_WARNINGS_FOR_GCC
 
 #ifdef __ANDROID__
-# define lseek lseek64
-# define open open64
-# define off_t off64_t
-
 # define DISABLE_SHM
 #endif
 
@@ -5631,21 +5627,11 @@ bool os::dir_is_empty(const char* path) {
 #define O_DELETE 0x10000
 #endif
 
-// [Android port] JDK9 mobile port:
-/*
 #ifdef __ANDROID__
 int open64(const char* pathName, int flags, int m
   return ::open(pathName, flags, mode);
 }
 #endif //__ANDROID__
-*/
-// That's weird, just seems calling to itself from os::open...
-// Also it gives error:
-/*
-openjdk/hotspot/src/os/linux/vm/os_linux.cpp:5638:51: error: no 'int os::open64(const char*, int, int)' member function declared in class 'os'
- int os::open(const char *path, int oflag, int mode) {
-                                                   ^
-*/
 
 // Open a file. Unlink the file immediately after open returns
 // if the specified oflag has the O_DELETE flag set.
@@ -5660,11 +5646,8 @@ int os::open(const char *path, int oflag, int mode) {
   int fd;
   int o_delete = (oflag & O_DELETE);
   oflag = oflag & ~O_DELETE;
-#ifndef __ANDROID__
+
   fd = ::open64(path, oflag, mode);
-#else
-  fd = open64(path, oflag, mode);
-#endif
   if (fd == -1) return -1;
 
   //If the open succeeded, the file might still be a directory
