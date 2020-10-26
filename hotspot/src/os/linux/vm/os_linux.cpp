e10ce187927192cb2244752f9d15ec96b15c8799
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-26 13:00:19 +0700

    Fx

--- hotspot/src/os/linux/vm/os_linux.cpp
+++ hotspot/src/os/linux/vm/os_linux.cpp
@@ -2456,7 +2456,7 @@ void os::jvm_path(char *buf, jint buflen) {
   if (java_home_var == NULL || dli_fname[0] == '\0') {
     return;
   }
-  snprintf(buf, buflen, "%s/lib/$s/server/%s", java_home_var, cpu_arch, dli_fname);
+  snprintf(buf, buflen, "%s/lib/%s/server/%s", java_home_var, cpu_arch, dli_fname);
 #else // !__ANDROID__
   char *rp = NULL;
   if (ret && dli_fname[0] != '\0') {
