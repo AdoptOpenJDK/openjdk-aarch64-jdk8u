25a5e958c0d7b3bb19a23962dd3119d43208c631
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-11-10 05:02:18 +0700

    Add aarch32

--- hotspot/src/share/vm/runtime/threadLocalStorage.hpp
+++ hotspot/src/share/vm/runtime/threadLocalStorage.hpp
@@ -51,6 +51,9 @@ class ThreadLocalStorage : AllStatic {
 #ifdef TARGET_OS_ARCH_linux_x86
 # include "threadLS_linux_x86.hpp"
 #endif
+#ifdef TARGET_OS_ARCH_linux_aarch32
+# include "threadLS_linux_aarch32.hpp"
+#endif
 #ifdef TARGET_OS_ARCH_linux_aarch64
 # include "threadLS_linux_aarch64.hpp"
 #endif
